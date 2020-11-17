4127fccc9afcf96d66ea0dcaaee1827ae8f658c3
HusamAjour <husamaj278@gmail.com>
2020-11-17 15:28:02 +0200

    lab-03 done

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
 'strict mode';
+
+$('#page1').on('click', loadPage1);
+$('#page2').on('click', loadPage2);
 let keywords = [];
-$.ajax('./data/page-1.json').then(data => {
-  data.forEach(element => {
-    if (!keywords.includes(element.keyword)) {
-      keywords.push(element.keyword);
-    }
-    let newAnimal = new Animal(element);
-    newAnimal.render();
+
+function loadPage1() {
+  prepareLoad();
+  $.ajax('./data/page-1.json').then(data => {
+    data.forEach(element => {
+      if (!keywords.includes(element.keyword)) {
+        keywords.push(element.keyword);
+      }
+      let newAnimal = new Animal(element);
+      newAnimal.render();
+    });
+    fillFilterList(keywords);
   });
-  fillFilterList(keywords);
-});
+}
+
+function loadPage2() {
+  prepareLoad();
+  $.ajax('./data/page-2.json').then(data => {
+    data.forEach(element => {
+      if (!keywords.includes(element.keyword)) {
+        keywords.push(element.keyword);
+      }
+      let newAnimal = new Animal(element);
+      newAnimal.render();
+    });
+    fillFilterList(keywords);
+  });
+}
+
+function prepareLoad(){
+  $('main').html('');
+  let defaultFilter = $('#filterMenu option').first();
+  $('#filterMenu').html('');
+  $('#sortMenu').val('default');
+  $('#filterMenu').append(defaultFilter);
+  Animal.all =[];
+  keywords = [];
+}
 
 Animal.all = [];
 function Animal(data) {
@@ -17,14 +48,19 @@ function Animal(data) {
   this.imageURL = data.image_url;
   this.desc = data.description;
   this.keyword = data.keyword;
+  this.horns = data.horns;
   Animal.all.push(this);
 }
 Animal.prototype.render = function () {
-  let animalCard = $('#photo-template').first().clone();
-  animalCard.find('h2').text(this.name);
-  animalCard.find('img').attr('src', this.imageURL);
-  animalCard.find('p').text(this.desc);
-  $('main').append(animalCard);
+  /*  let animalCard = $('#photo-template').first().clone();
+    animalCard.find('h2').text(this.name);
+    animalCard.find('img').attr('src', this.imageURL);
+    animalCard.find('p').text(this.desc);
+    $('main').append(animalCard);
+  */
+  let temp = $('#aimalCardTemplate').html();
+  let cardHtml = Mustache.render(temp, this); //(string,object)
+  $('main').append(cardHtml);
 };
 
 
@@ -33,11 +69,11 @@ function fillFilterList(keywords) {
     let option = $(`<option></option>`);
     option.attr('value', kw);
     option.text(kw);
-    $('select').append(option);
+    $('#filterMenu').append(option);
   });
 }
 
-$('select').on('change', function () {
+$('#filterMenu').on('change', function () {
   let filterValue = $(this).val();
   if (filterValue !== 'default') {
     let animalCard = $('#photo-template').first();
@@ -48,10 +84,42 @@ $('select').on('change', function () {
         animal.render();
       }
     });
-  } else{
+  } else {
     let animalCard = $('#photo-template').first();
     $('main').html('');
     $('main').append(animalCard);
+    Animal.all.forEach(animal => {
+      animal.render();
+    });
+  }
+});
+
+
+$('#sortMenu').on('change', function () {
+  let sortValue = $(this).val();
+  if (sortValue !== 'default') {
+    $('main').html('');
+    if(sortValue === 'title'){
+      Animal.all.sort( (a,b) => {
+        if (a.name.toUpperCase() > b.name.toUpperCase()) {
+          return 1;
+        } else if (a.name.toUpperCase() < b.name.toUpperCase()) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    } else if(sortValue === 'horns'){
+      Animal.all.sort( (a,b) => {
+        if (a.horns > b.horns) {
+          return 1;
+        } else if (a.horns < b.horns) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    }
     Animal.all.forEach(animal => {
       animal.render();
     });
